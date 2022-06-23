019d18879ee844effbe0f5230acab66103ee983a
andrefcg1 <65246453+andrefcg1@users.noreply.github.com>
2022-06-23 11:31:30 -0300

    hart2dart

--- hart2dart/hrtTypeHex2Time.dart
+++ hart2dart/hrtTypeHex2Time.dart
@@ -1,19 +1,15 @@
-//global
-// function strTime=hrtTypeHex2Time(strHex)//'XX XX XX XX' -> 0.03125
-// var auxVet = hex2dec(tokens(strHex, ' '));
-// var valor =
-//     auxVet(1) * 524288 + auxVet(2) * 2048 + auxVet(3) * 8 + auxVet(4) * 0.03125;
-// var modulo = valor % 1000;
+//Parâmetros
+var ConverttoMs = 0.03125;
+var ConverttoHours = 3600000;
 
-String hx = '0x7FFFFFFF';
-var hex = hx.substring(2);
-int strHex = int.parse(hex, radix: 16);
-int strHex1 = int.parse('7F', radix: 16);
-int strHex2 = int.parse('FF', radix: 16);
-int strHex3 = int.parse('FF', radix: 16);
-int strHex4 = int.parse('FF', radix: 16);
-
-var convertHex = (strHex * 0.03125 / 3600000);
+//Global
+String hx = '0xFFFFFFFF'; // String em hexadecimal - escala "Unsigned"
+var hex = hx.substring(2); //Remove os dois primeiros caracteres da string
+int strHex = int.parse(hex,
+    radix: 16); //Valor converitdo com range de 0 a 4294967295, em decimal
+var convertHex = (strHex *
+    ConverttoMs /
+    ConverttoHours); //Aplica conversões do valor decimal para obter as horas
 
 void main() {
   print('$hx          (Valor Hexadecimal)'); //Valor Hexadecimal
@@ -21,6 +17,5 @@ void main() {
       '$hex            (Valor Hexadecimal pronto para converter para decimal)'); // Valor Hexadecimal pronto para converter para decimal
   print(
       '$strHex          (Valor Convertido para decimal)'); //Valor Convertido para decimal
-  print('\nValor em Horas é : $convertHex \n');
-  print(strHex1 * 524288 / 3600000);
+  print('\nValor em Horas é: $convertHex \n');
 }
